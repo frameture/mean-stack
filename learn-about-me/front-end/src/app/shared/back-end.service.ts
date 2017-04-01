b2caa145dc66760c02583a32e7a49799c7104c3e
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-01 11:37:49 +0200

    talking to the back-end api

--- learn-about-me/front-end/src/app/shared/back-end.service.ts
+++ learn-about-me/front-end/src/app/shared/back-end.service.ts
@@ -1,16 +1,30 @@
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
+
 import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/switchMap';
 
 @Injectable()
 export class BackEndService {
 
-  private readonly URL = 'http://localhost:3333';
+  private readonly URL = 'http://localhost:3334';
 
   constructor(private http: Http) { }
 
-  checkServer(): Observable<any> {
-    return this.http.get(this.URL);
+  getUser(username: string): Observable<any> {
+    return this.http.get(this.URL + '/user/' + username);
+  }
+
+  getUsers(): Observable<any> {
+    return this.http.get(this.URL + '/users');
+  }
+
+  register(user: { username: string, password: string }): Observable<any> {
+    let data = JSON.stringify(user);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.URL + '/register', { data }, options);
   }
 
 }
