056fe2b246ee78a897943d4667c7f8e3a41dc31a
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-02 16:20:22 +0200

    login form

--- learn-about-me/front-end/src/app/login/login-form/login-form.component.ts
+++ learn-about-me/front-end/src/app/login/login-form/login-form.component.ts
@@ -1,15 +1,67 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: [ './login-form.component.css' ]
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  errorInfo: string;
+
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  onLogout(): void {
+    this.userService.logout();
+  }
+
+  onReset(): void {
+    this.errorInfo = '';
+    this.loginForm.reset({
+      username: '',
+      login: ''
+    });
+  }
+
+  onSubmit(): void {
+    const controls = this.getControls();
+    this.userService
+      .login(controls)
+      .subscribe(res => this.handleResponse(res), console.error);
+  }
+
+  private createForm(): void {
+    this.loginForm = this.fb.group({
+      username: '',
+      password: ''
+    });
+  }
+
+  private getControls(): any {
+    return {
+      username: this.loginForm.get('username').value,
+      password: this.loginForm.get('password').value,
+    };
+  }
+
+  private handleResponse(res) {
+    if (res.success) {
+      this.errorInfo = '';
+      this.userService.setUser(res.user);
+      return;
+    }
+    this.errorInfo = res.message;
   }
 
 }
