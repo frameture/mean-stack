ee7c7f5eeef9e2935973c557830ed4aeb1f960b5
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-02 16:23:39 +0200

    restructuring

--- learn-about-me/back-end/src/app.ts
+++ learn-about-me/back-end/src/app.ts
@@ -1,141 +1,38 @@
-import * as express from 'express';
-import * as logger from 'morgan';
-import * as mongoose from 'mongoose';
 import * as bodyParser from 'body-parser';
-import * as qs from 'querystring';
-import * as cookieParser from 'cookie-parser';
-import { Express } from "express";
-import { Model, Document } from 'mongoose';
+import * as cors from 'cors';
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as logger from 'morgan';
 
 import User from './models/user';
-
-declare let logger: any;
+import Routes from './routes';
 
 export default class App {
 
-  private app: Express;
-  private model: Model<Document>;
-  private readonly PORT = 3334;
-  private readonly DB = 'mongodb://localhost:27017/test'
+  private app: express.Express;
+  private router: express.Router;
+  private readonly PORT = 3001;
+  private readonly DB = 'mongodb://localhost:27017/test';
+
+  constructor() {
+    this.app = express();
+    this.router = new Routes().getRouter();
+  }
 
   start(): void {
-    this.setApp();
-    this.setModel();
-    this.startApp();
-  }
-
-  private setGetUser(): void {
-
-    this.app.get('/user/:username', (req, res) => {
-      let username = req.params.username;
-      this.model.findOne({ username: username }, (err, user) => {
-        if (err) {
-          console.error(err);
-          return res.end(JSON.stringify({ error: err }));
-        }
-        if (user) {
-          res.end(JSON.stringify(user));
-        } else {
-          res.end(JSON.stringify('No user'));
-        }
-      })
-      res.writeHead(200, {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Content-Type": "application/json"
-      });
-    });
-  }
-
-  private setGetUsers(): void {
-    this.app.get('/users', (req, res) => {
-      this.model
-        .find()
-        .sort({ createdAt: 'descending' })
-        .exec((err, users) => {
-          if (err) {
-            console.error(err);
-            res.end(JSON.stringify({ error: err }));
-            return;
-          }
-          if (users.length) {
-            res.end(JSON.stringify(users));
-          } else {
-            res.end(JSON.stringify('No users'));
-          }
-        });
-      res.writeHead(200, {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Content-Type": "application/json"
-      });
-    });
-  }
-
-  private setPost(): void {
-    this.app.options('*', (req: express.Request, res: express.Response, next) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-      res.setHeader('Content-Type', 'application/json');
-      next();
-    });
-
-    this.app.post('/register', (req: express.Request, res: express.Response) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Request-Headers', 'Content-Type');
-      res.setHeader('Content-Type', 'application/json');
-
-      let data = JSON.parse(req.body.data);
-      let username = data.username;
-      let password = data.password;
-
-      this.model.findOne({ username: username }, (err, user) => {
-        if (err) {
-          console.error('error');
-          return res.status(500).end('Internal Error');
-        }
-        if (user) {
-          return res.status(409).end(JSON.stringify(
-            { error: 'User does already exist with that username.' }
-          ));
-        }
-        const newUser = new this.model({
-          username: username,
-          password: password
-        });
-        newUser.save((err) => {
-          if (err) {
-            console.error('error while saving document', err)
-            return res.status(500).end('Internal Error');
-          }
-        });
-        res.status(200).end('New account registered');
-      })
-    });
-
-  }
-
-  private setApp(): void {
-    this.app = express();
+    this.app.use(cors());
     this.app.use(logger('dev'));
     this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(bodyParser.json());
-    this.app.use(cookieParser());
 
-    this.setGetUser();
-    this.setGetUsers();
-    this.setPost();
-    // login
+    // this.app.use(passport.initialize());
+    // this.app.use(passport.session());
+    this.app.use(this.router);
 
-  }
+    mongoose.connect(this.DB);
 
-  private setModel(): void {
-    mongoose.connect(this.DB)
-    this.model = new User().getModel();
-  }
-
-  private startApp(): void {
     this.app.listen(this.PORT, () =>
       console.log('server started on port ' + this.PORT));
   }
+
 }
---
+++ learn-about-me/back-end/src/routes.ts
@@ -0,0 +1,131 @@
+import * as express from 'express';
+import * as jwt from 'express-jwt';
+import { Document, Model } from 'mongoose';
+import * as passport from 'passport';
+import { Strategy } from 'passport-local';
+
+import User from './models/user';
+
+export default class Routes {
+
+  private authCheck: jwt.RequestHandler;
+  private user: Model<Document>;
+  private router: express.Router;
+
+  constructor() {
+    this.authCheck = jwt({
+      secret: new Buffer('9k5MqFPD6XM2LRjoYACIjvIWkgr5LYBoNjzWdBG7AhD5dfCvr7zqoL3IDneH-oZT', 'base64'),
+      audience: 'DAjXyietLOcv9SEaV23wx4ejrix9E3UB'
+    });
+    this.user = new User().getModel();
+    this.router = express.Router();
+  }
+
+  getRouter(): express.Router {
+    this.setRoutes();
+    return this.router;
+  }
+
+  private setRoutes(): void {
+    this.routeLogin();
+    this.routeUser();
+    this.routeUsers();
+    this.routeRegister();
+  }
+
+  private routeUser(): void {
+    this.router.get('/user/:username', (req, res) => {
+      const username = req.params.username;
+      this.user.findOne({ username }, (err, user) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ message: 'Internal Server Error' });
+        }
+        if (user) {
+          res.json(user);
+        } else {
+          res.json('No user');
+        }
+      });
+    });
+  }
+
+  private routeUsers(): void {
+    this.router.get('/users', (req, res) => {
+      this.user
+        .find()
+        .sort({ createdAt: 'descending' })
+        .exec((err, users) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Internal Server Error' });
+          }
+          if (users.length) {
+            res.json(users);
+          } else {
+            res.json('No users');
+          }
+        });
+    });
+  }
+
+  private routeLogin(): void {
+    this.router.post('/login', (req, res) => {
+      const user = JSON.parse(req.body.data);
+      this.user.findOne({ username: user.username }, (err, foundUser: any) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).end('Internal Server Error');
+        }
+        if (!foundUser) {
+          return res.json({ success: false, message: 'Wrong username.' });
+        }
+
+        foundUser.checkPassword(user.password, (err, isMatch) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).end('Internal Server Error');
+          }
+          if (!isMatch) {
+            res.json({ success: false, message: 'Wrong password.' });
+          } else {
+            const auth_token = 'some-random-token';
+            res.json({ success: true, user: foundUser, auth_token });
+          }
+        });
+      });
+    });
+  }
+
+  private routeRegister(): void {
+    this.router.post('/register', (req: express.Request, res: express.Response) => {
+      const data = JSON.parse(req.body.data);
+      const username = data.username;
+      const password = data.password;
+
+      this.user.findOne({ username }, (err, user) => {
+        if (err) {
+          console.error('error');
+          return res.status(500).end('Internal Server Error');
+        }
+        if (user) {
+          return res.status(409).json(
+            { message: 'User does already exist with that username.' },
+          );
+        }
+        const newUser = new this.user({
+          username,
+          password,
+        });
+        newUser.save((err) => {
+          if (err) {
+            console.error('error while saving document', err);
+            return res.status(500).end('Internal Server Error');
+          }
+        });
+        res.status(200).send('New account registered');
+      });
+    });
+
+  }
+}
